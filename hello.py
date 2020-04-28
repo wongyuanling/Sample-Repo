e2137f4661e81ca21bcce02fbd12d0ec7cc751b2
wongyuanling <47958742+wongyuanling@users.noreply.github.com>
2020-04-28 13:53:41 +0800

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,64 @@
+import tkinter as tk
+from tkinter import filedialog, Text
+import os
+import requests
+
+r = requests.get('https://coreyms.com')
+print(r.status_code)
+
+name = input("Your name?")
+print("Hello, ", name)
+
+root = tk.Tk()
+apps = []
+
+if os.path.isfile('save.txt'):
+    with open('save.txt', 'r') as f:
+        tempApps = f.read()
+        # print(tempApps)
+        tempApps = tempApps.split(',')
+        apps = [x for x in tempApps if x.strip()]
+
+
+def addApp():
+
+    for widget in frame.winfo_children():
+        widget.destroy()
+
+    filename = filedialog.askopenfilename(
+        initialdir="/", title="Select File", filetypes=(("executables", "*.exe"), ("all files", "*.*")))
+    apps.append(filename)
+    print(filename)
+    for app in apps:
+        label = tk.Label(frame, text=app, bg="gray")
+        label.pack()
+
+
+def runApps():
+    for app in apps:
+        os.startfile(app)
+
+
+canvas = tk.Canvas(root, height=400, width=400, bg="blue")
+canvas.pack()
+
+frame = tk.Frame(root, bg="white")
+frame.place(relwidth=0.8, relheight=0.6, relx=0.1, rely=0.1)
+
+openFile = tk.Button(root, text="Open File", padx=10, pady=5,
+                     fg="white", bg="black", command=addApp)
+openFile.pack()
+
+runApps = tk.Button(root, text="Run Apps", padx=10, pady=5,
+                    fg="white", bg="black", command=runApps)
+runApps.pack()
+
+for app in apps:
+    label = tk.Label(frame, text=app)
+    label.pack()
+
+root.mainloop()
+
+with open('save.txt', 'w') as f:
+    for app in apps:
+        f.write(app + ',')
